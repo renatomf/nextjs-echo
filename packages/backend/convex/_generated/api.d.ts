aeed41bbabae189655d26b886143bc1809540982
renatomarf76 <renatomarf76@egmail.com>
2025-10-01 16:55:25 -0300

    10: Widget Session

--- packages/backend/convex/_generated/api.d.ts
+++ packages/backend/convex/_generated/api.d.ts
@@ -13,6 +13,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as public_contactSession from "../public/contactSession.js";
 import type * as users from "../users.js";
 
 /**
@@ -24,6 +25,7 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  "public/contactSession": typeof public_contactSession;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
